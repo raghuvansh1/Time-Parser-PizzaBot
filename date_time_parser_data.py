0f12f7569ef4b9fe0258f762ed15fd9cc1458e81
Aniket Agarwal <agarwalaniket1999@gmail.com>
2018-02-08 22:29:01 +0530

    Fixed a typo in the data set

--- date_time_parser_data.py
+++ date_time_parser_data.py
@@ -9,7 +9,7 @@ obj_s={'days':1,'dates':1,'weeks':7,'fortnights':14,'months':1,'years':1,
        'decades':10,'centuries':100}
 
 multi={'before':-1,'after':1,'later':1,'next':1,'previous':-1,'this':0,'last':-1,
-      'earlier':-1,'comming':0}
+      'earlier':-1,'coming':0}
 
 
 drx_obj={'today':0,'tomorrow':1,'yesterday':-1}
